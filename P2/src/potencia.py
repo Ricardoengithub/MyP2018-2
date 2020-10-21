280c5acc10a81601d0c54061b141d29a80c55e07
Ricardo RC <gaanzz11@gmail.com>
2020-10-21 02:08:09 +0000

    Update potencia.py

--- P2/src/potencia.py
+++ P2/src/potencia.py
@@ -1,12 +1,18 @@
 def potencia(x,y):
-	if (y == 0):	
+	if (y == 0):
 		return 1
 	elif (y == 1):
 		return x
 	else:
 		return x * potencia(x,y-1)
-		
-x = int(input("Escribe x: "))
-y = int(input("Escribe la potencia a la que quieres elevar x: "))
 
-print(potencia(x,y))		
+def main():
+
+	x = int(input("Escribe x: "))
+	y = int(input("Escribe la potencia a la que quieres elevar x: "))
+
+	print(f"La potencida de {x} elevado a {y} es: {potencia(x,y)}")
+
+
+if __name__ == "__main__":
+    main()
